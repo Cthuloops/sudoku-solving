28c7758a8eff29bec01559c2fea96136fae35c68
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-10-13 19:39:11 -0400

    feat(cell): remove count from cell add count lookup table.

--- include/cell.h
+++ include/cell.h
@@ -17,11 +17,11 @@
 enum Entropy {zero, one, two, three, four, five, six, seven, eight, nine, all};
 extern const uint16_t entropy_masks[enum_entropy_size];
 extern const enum Entropy entropies[enum_entropy_size];
-#define entropy_count_mask 0b0001111000000000
+#define entropy_lookup_amount 512
+extern uint8_t entropy_lookup[entropy_lookup_amount];
 
+void initialize_entropy_lookup(void);
 static inline size_t get_entropy_count(uint16_t cell);
-static inline size_t calculate_entropy_count(uint16_t cell);
-static inline void recalculate_entropy_count(uint16_t *cell);
 static inline void remove_entropy_value(uint16_t *cell, enum Entropy value);
 static inline uint16_t get_initialized_cell(void);
 static inline bool is_valid_entropy(uint16_t cell, enum Entropy entropy);
@@ -30,6 +30,7 @@ static inline int8_t collapse(uint16_t *cell, enum Entropy entropy);
 /**
  * @brief Get_entropy_values places the available values for a cell in a buffer.
  * @returns The count of elements placed within the buffer.
+ * @note If the cell or buf is NULL, does nothing and returns 0.
  */
 uint8_t get_entropy_values(uint16_t *cell, enum Entropy buf[]);
 
@@ -38,38 +39,8 @@ uint8_t get_entropy_values(uint16_t *cell, enum Entropy buf[]);
  * @returns The count of entropy.
  */
 static inline size_t get_entropy_count(uint16_t cell) {
-    cell &= entropy_count_mask;
-    return cell >> 9;
-}
-
-/**
- * @brief Count the number of bits set within the last 9 bits of the cell.
- * @param cell The cell to count bits.
- * @returns The amount of bits counted - 1.
- */
-static inline size_t calculate_entropy_count(uint16_t cell) {
-    size_t count = 0;
-    // Wipe the flags except available entropy values.
-    cell &= entropy_masks[all];
-    while (cell != 0) {
-        count += (cell & 1);  // See if the least significant bit is set.
-        cell >>= 1;  // Shift the pattern down 1.
-    }
-    return count - 1;
-}
-
-/**
- * @brief Calculates and updates entropy count.
- * @param cell Pointer to the cell.
- * @note If cell is NULL, the function returns without action.
- */
-static inline void recalculate_entropy_count(uint16_t *cell) {
-    if (cell == NULL) {
-        return;
-    }
-    *cell &= entropy_masks[all];
-    size_t count = calculate_entropy_count(*cell);
-    *cell |= (count << 9);
+    // Grab only the 9 lsbs.
+    return entropy_lookup[cell & entropy_masks[all]];
 }
 
 /**
@@ -86,18 +57,15 @@ static inline void remove_entropy_value(uint16_t *cell, enum Entropy value) {
 
     if (is_valid_entropy(*cell, value)) {
         *cell &= ~entropy_masks[value];
-        recalculate_entropy_count(cell);
     }
 }
 
 /**
- * @brief Get initialized cell (all possible values + count).
+ * @brief Get initialized cell (all possible values).
  * @returns A initialized cell.
  */
 static inline uint16_t get_initialized_cell(void) {
-    uint16_t cell = entropy_masks[all];
-    recalculate_entropy_count(&cell);
-    return cell;
+    return (uint16_t)entropy_masks[all];
 }
 
 
@@ -106,14 +74,15 @@ static inline uint16_t get_initialized_cell(void) {
  * @returns true if present, false otherwise.
  */
 static inline bool is_valid_entropy(uint16_t cell, enum Entropy entropy) {
-    return cell & entropy_masks[entropy];
+    // cast to bool since explicit is better than implicit.
+    return (bool)(cell & entropy_masks[entropy]);
 }
 
 
 /**
- * @brief Collapse collapses a cell to a set value and sets it's count to 0.
+ * @brief Collapse collapses a cell to a set value.
  * @param cell Pointer to the cell to collapse.
- * @param entropy The entropy state to set (zero through nine)
+ * @param entropy The entropy state to set (one through nine)
  * @returns The numeric value (1-9) corresponding to the entropy.
  *          INT8_MAX if value is 'all' (invalid input).
  *          -1 if the value is not a valid entropy for the cell.
